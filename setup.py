e96d60e00ae8f0dee57a83ba5cc4b612231c358e
kz159 <loh@kz159.ru>
2020-05-24 02:48:40 +0300

    renamed folders

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
 import os 
 #from capturica_db import __version__
-from setuptools import setup
 
+from setuptools import setup
+from walld_db import __version__
 reqs_file = os.path.join(os.path.dirname(__file__), 'requirements.txt')
 
 link = 'git+git://github.com/kz159/WalldDataBase.git'
 
 setup(name='walld_db',
-      version='0.1',
+      version=__version__,
       description='represents walld db tables',
       url=link,
       author='kz159',
       author_email='loh@kz159.ru',
-      package_dir={'': 'walld_db'},
+      # package_dir={'': 'walld_db'},
       license='GNU',
       packages=['walld_db'],
       install_requires=[s.strip() for s in open(reqs_file).readlines()])
